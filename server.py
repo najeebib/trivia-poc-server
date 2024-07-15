fbfc48a1f4066aae868fca392d0045a1c95c3c96
najeebib <najeebibraheem1999@hotmail.com>
2024-07-15 10:07:04 +0300

    server and data init

---
+++ server.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Request
+from routes import game_routes
+app = FastAPI()
+
+app.include_router(game_routes.router)
+
+@app.middleware("http")
+async def log_req(request:Request, call_next):
+    print(f'got req. to: {request.url}, method: {request.method}')
+    response = await call_next(request)
+    return response
+
+@app.get("/")
+def root():
+    return "hi from fast api"
